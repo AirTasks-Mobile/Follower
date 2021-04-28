fac510de63d84b383cc468b79368d8016a827367
Jeremy Nguyen <nongdancuibap@gmail.com>
2021-04-28 15:39:07 +0800

    Initial Commit

---
+++ Followers/FollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersApp.swift
+//  Followers
+//
+//  Created by JEREMY NGUYEN on 28/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
